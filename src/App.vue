f6ddf93f9117bb82c92dfd0ddd710bc9a42cb57c
DaviBisewski <davimbisewski@gmail.com>
2024-07-22 16:53:26 -0300

    inicio do projeto

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Dados from './components/Dados.vue';
+import Form from './components/Form.vue';
+
+
+</script>
+
+<template>
+
+  <Dados></Dados>
+  <Form></Form>
+ 
+</template>
+
+<style scoped>
+
+</style>
